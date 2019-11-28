e973b83c02109c321bda5d4d216851f1f53cb8bf
MichaelVolynskiyBC <michaelvolynskiy1@gmail.com>
2019-11-28 14:49:57 -0500

    Pushing my code and related files to Assignment_3 repository

---
+++ FrequencyList.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.Integer;
+
+public class FrequencyList {     // The input files should be in the same folder as the code, and all output files will be generated inside the folder
+    public static void main(String[] args) throws IOException {     // Thrown exception for if a file is not found
+        Scanner sc = new Scanner(new File("lyrics.txt"));       // File object passed to a Scanner
+        PrintWriter pw = new PrintWriter("output.txt");         // PrintWriter creates a file for the finalized output
+        ArrayList<String> wordList = new ArrayList<String>();       // the list that will be populated with the words in the file
+        ArrayList<String> outputList = new ArrayList<String>();     // a formatted list with the intended output structure
+
+        // while there are more words to be read in: each input's special characters are replaced with an empty string, 
+        // numbers replaced by an empty string, and lower cased, finally all stop words are removed (replaced with "null")
+        while (sc.hasNext()) { 
+            String word = removeStopWords(sc.next().replaceAll("[^a-zA-Z0-9]", "").replaceAll("[0-9]", "").toLowerCase());
+
+            if (!word.equals("null") && !word.equals(""))       // If the input is not a stopword [determined by removeStopWords(String)], and is not an empty string, it is added to the the wordList
+                wordList.add(word);
+        }
+
+        sc.close();     // Scanner is closed
+
+        for (int i = 0; i < wordList.size(); i++)       // For every element in the wordList, the outputList will be populated with a formatted output (Frequency: Word)
+            outputList.add(frequency(wordList, wordList.get(i)) + ": " + wordList.get(i));
+
+        for (int i = 0; i < sortedList(outputList).size(); i++)      // the sortedList will be printed to the file created by the PrintWriter
+            pw.println(sortedList(outputList).get(i));
+
+        pw.print("\nThe total number of unique, non-stopwords in this file is: " + sortedList(outputList).size());      // The total number of unique, non-stop words in the file are printed
+        
+        pw.close();     // PrintWriter is closed
+    } // end of main method
+
+    public static int frequency(ArrayList<String> list, String word) {      // If the inputted word matches a word in the wordList, count is incremented and returned
+        int count = 0;
+
+        for (int i = 0; i < list.size(); i++)
+            if (word.equals(list.get(i)))
+                count++;
+
+        return count;
+    }
+
+    // A modified linear search algorithm that removes the element at index j from the wordList if it matches the contents of the element at index i
+    public static void removeDupes(ArrayList<String> list) {
+        for (int i = 0; i < list.size() - 1; i++)
+            for (int j = i + 1; j < list.size(); j++)
+                if (list.get(i).equals(list.get(j)))
+                    list.remove(j);
+    }
+
+    // A sortedList that is populated with the sorted inputs (sorted in descending order by frequency) of the outputList, after the sortedList's duplicate inputs are removed
+    public static ArrayList<String> sortedList(ArrayList<String> list) { 
+        ArrayList<String> tempList = list;
+
+        for (int i = 0; i < tempList.size() - 1; i++)
+            for (int j = i + 1; j < tempList.size(); j++) {
+                String tempI = tempList.get(i);     // Temporary variables that store the elements at indexes i and j
+                String tempJ = tempList.get(j);
+
+                // If the value of the substring of the temporary variable, tempI (from the first character, up to the index of a colon), as an integer is less than 
+                // the value of the substring of the temporary variable, tempJ, as an integer, then the two variables' index and value are swapped
+                if (Integer.valueOf(tempI.substring(0, tempI.indexOf(":"))) < Integer.valueOf(tempJ.substring(0, tempJ.indexOf(":")))) {
+                    tempList.set(i, tempJ);
+                    tempList.set(j, tempI);
+                }
+            }
+            
+            removeDupes(tempList);      // duplicate words and their frequency values are removed from the tempList
+
+            return tempList;        // the tempList, after it is sorted and duplicates are removed, is returned
+    }
+
+    // A stopWordList is populated with all the words in the file (with all special characters removed and lowercased), then if a word in the stopWordList matches a word (input), "null" will be returned
+    public static String removeStopWords(String input) throws IOException {     // IOException is thrown in case the stopwords.txt file is missing
+        ArrayList<String> stopWordList = new ArrayList<String>();
+        Scanner sc = new Scanner(new File("stopwords.txt"));
+
+        while (sc.hasNext()) {
+            String word = sc.next().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+            stopWordList.add(word);
+        }
+
+        sc.close();
+
+        for (int i = 0; i < stopWordList.size(); i++)
+            if (input.substring(0, input.length()).equals(stopWordList.get(i)))
+                return "null";
+
+        return input;   // if the word is not a stop word, it will be returned
+    }
+} // End of class FrequencyList 
---
+++ README.md
@@ -0,0 +1,29 @@
+# CISC 3130 - Data Structures - Assignment 3
+
+
+Instructor: Katherine Chuang \
+Brooklyn College
+
+
+## Objectives
+
+1.  Prepare a text file that contains text to analyze. It could be song lyrics to your favorite song. 
+2.  With your code, you’ll read from the text file and capture the data into a data structure. 
+3.  Using a data structure, write the code to count the appearance of each unique word in the lyrics.
+4.  Print out a word frequency list. 
+
+**Sample Output:** 
+
+`100: frog`
+`94: dog`
+`43: cog`
+`20: bog`
+
+### Extra Objectives:
+
+* Consider whether you want your keys to be case sensitive
+* Consider whether you want to include common stopwords such as the, a, is (more can be found at https://www.ranks.nl/stopwords)
+
+---
+
+Suggested Implementation Code: https://github.com/bc-cisc3130-ty2-fall2019/data-structures-java/blob/master/maven_example/word_cloud/src/main/java/myApp/WordCount.java
